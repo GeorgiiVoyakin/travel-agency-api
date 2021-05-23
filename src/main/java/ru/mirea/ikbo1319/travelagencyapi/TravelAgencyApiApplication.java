480fab452790f413e5f88d450bd56a9d94c38267
GeorgiiVoyakin <voyaking@gmail.com>
2021-05-23 18:29:47 +0300

    spring boot initialization

---
+++ src/main/java/ru/mirea/ikbo1319/travelagencyapi/TravelAgencyApiApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo1319.travelagencyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mirea/ikbo1319/travelagencyapi/TravelAgencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo1319.travelagencyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
