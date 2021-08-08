d80f18c56d7f82de721b4eb78d29bd27da05c297
Georgii Voyakin <voyaking@gmail.com>
2021-08-08 18:14:59 +0300

    added City model

---
+++ src/main/java/ru/mirea/ikbo1319/model/City.java
@@ -0,0 +1,16 @@
+package ru.mirea.ikbo1319.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class City {
+    private @Id @GeneratedValue Long id;
+    private String name;
+
+    public City() {}
+}
