8befe375d648f5370f74dfe244b8e26681e28c0c
Georgii Voyakin <voyaking@gmail.com>
2021-08-08 18:51:42 +0300

    updated models GeneratedValue strategy set to IDENTITY

--- src/main/java/ru/mirea/ikbo1319/model/City.java
+++ src/main/java/ru/mirea/ikbo1319/model/City.java
@@ -4,12 +4,13 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class City {
-    private @Id @GeneratedValue Long id;
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
     private String name;
 
     public City() {}
