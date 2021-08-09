cd6077d7c0e70fee082c60888839cb637d6350f5
Georgii Voyakin <voyaking@gmail.com>
2021-08-09 18:56:25 +0300

    added Tour repository

---
+++ src/main/java/ru/mirea/ikbo1319/repository/TourRepository.java
@@ -0,0 +1,6 @@
+package ru.mirea.ikbo1319.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.mirea.ikbo1319.model.Tour;
+
+public interface TourRepository extends JpaRepository<Tour, Long> {}
