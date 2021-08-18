9dea930c8cdcfb83aa5287b57b881a59ae3342c4
Georgii Voyakin <voyaking@gmail.com>
2021-08-18 17:30:59 +0300

    added response status in exceptions

--- src/main/java/ru/mirea/ikbo1319/exception/CityNotFoundException.java
+++ src/main/java/ru/mirea/ikbo1319/exception/CityNotFoundException.java
@@ -1,5 +1,9 @@
 package ru.mirea.ikbo1319.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CityNotFoundException extends RuntimeException {
     public CityNotFoundException(Long id) {
         super("Could not find city with id: " + id);
--- src/main/java/ru/mirea/ikbo1319/exception/TourNotFoundException.java
+++ src/main/java/ru/mirea/ikbo1319/exception/TourNotFoundException.java
@@ -1,5 +1,9 @@
 package ru.mirea.ikbo1319.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TourNotFoundException extends RuntimeException {
     public TourNotFoundException(Long id) {
         super("Could not find tour with id: " + id);
