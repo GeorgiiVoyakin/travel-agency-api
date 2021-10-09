48591782d1ac05754ad240dda1d5a0e2ce3caef0
Георгий Воякин <voyaking@gmail.com>
2021-10-09 21:26:29 +0300

    Added TourService in utils

--- src/main/java/ru/mirea/ikbo1319/controller/ControllersUtils.java
+++ src/main/java/ru/mirea/ikbo1319/controller/ControllersUtils.java
@@ -3,16 +3,20 @@ package ru.mirea.ikbo1319.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.mirea.ikbo1319.exception.CountryNotFoundException;
+import ru.mirea.ikbo1319.exception.TourNotFoundException;
 import ru.mirea.ikbo1319.model.City;
 import ru.mirea.ikbo1319.model.Country;
+import ru.mirea.ikbo1319.model.Tour;
 import ru.mirea.ikbo1319.service.CityService;
 import ru.mirea.ikbo1319.service.CountryService;
+import ru.mirea.ikbo1319.service.TourService;
 
 @Component
 @AllArgsConstructor
 public class ControllersUtils {
     private final CityService cityService;
     private final CountryService countryService;
+    private final TourService tourService;
 
     public City getCityOrThrowNotFound(Long cityId) {
         return cityService.findById(cityId)
@@ -22,5 +26,10 @@ public class ControllersUtils {
     public Country getCountryOrThrowNotFound(Long countryId) {
         return countryService.findById(countryId)
                 .orElseThrow(() -> new CountryNotFoundException(countryId));
+    }
+
+    public Tour getTourOrThrowNotFound(Long tourId) {
+        return tourService.findById(tourId)
+                .orElseThrow(() -> new TourNotFoundException(tourId));
     }
 }
