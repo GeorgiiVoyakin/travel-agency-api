3bf7c76500265e95c4cc86b02350c9c243c97459
Georgii Voyakin <voyaking@gmail.com>
2021-10-10 04:44:37 +0300

    added tour dto instead of db entity, removed find method

--- src/main/java/ru/mirea/ikbo1319/controller/TourController.java
+++ src/main/java/ru/mirea/ikbo1319/controller/TourController.java
@@ -1,67 +1,42 @@
 package ru.mirea.ikbo1319.controller;
 
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.http.HttpStatus;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.mirea.ikbo1319.config.TourModelAssembler;
 import ru.mirea.ikbo1319.dto.TourDto;
-import ru.mirea.ikbo1319.exception.CityNotFoundException;
-import ru.mirea.ikbo1319.exception.CountryNotFoundException;
 import ru.mirea.ikbo1319.exception.TourNotFoundException;
+import ru.mirea.ikbo1319.factory.TourDtoFactory;
 import ru.mirea.ikbo1319.model.Beach;
-import ru.mirea.ikbo1319.model.City;
 import ru.mirea.ikbo1319.model.Country;
 import ru.mirea.ikbo1319.model.Tour;
-import ru.mirea.ikbo1319.service.CityService;
-import ru.mirea.ikbo1319.service.CountryService;
 import ru.mirea.ikbo1319.service.TourService;
 
-import java.time.Instant;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@AllArgsConstructor
 public class TourController {
     private final TourService tourService;
-    private final CountryService countryService;
-    private final CityService cityService;
     private final TourModelAssembler tourModelAssembler;
-
-    public TourController(
-            TourService tourService,
-            CountryService countryService,
-            CityService cityService,
-            TourModelAssembler tourModelAssembler) {
-
-        this.tourService = tourService;
-        this.countryService = countryService;
-        this.cityService = cityService;
-        this.tourModelAssembler = tourModelAssembler;
-    }
+    private final TourDtoFactory tourDtoFactory;
+    private final ControllersUtils controllersUtils;
 
     @GetMapping("/tours")
-    public CollectionModel<EntityModel<Tour>> getAll() {
-        List<EntityModel<Tour>> tours = tourService
-                .findAll()
-                .stream()
-                .map(tourModelAssembler::toModel)
-                .collect(Collectors.toList());
+    public List<TourDto> getAll() {
+        List<Tour> tours = tourService.findAll();
 
-        return CollectionModel.of(tours, linkTo(methodOn(TourController.class).getAll()).withSelfRel());
+        return tourDtoFactory.createListOfTourDto(tours);
     }
 
     @GetMapping("/tours/{id}")
-    public EntityModel<Tour> getById(@PathVariable Long id) {
-        return tourModelAssembler
-                .toModel(tourService
+    public TourDto getById(@PathVariable Long id) {
+        return tourDtoFactory
+                .createTourDto(tourService
                         .findById(id)
                         .orElseThrow(() -> new TourNotFoundException(id)));
     }
@@ -71,73 +46,39 @@ public class TourController {
             @RequestParam String name,
             @RequestParam String description,
             @RequestParam Long price,
-            @RequestParam short distance,
+            @RequestParam Optional<Short> optionalDistance,
             @RequestParam boolean seaResort,
-            @RequestParam Beach beach,
+            @RequestParam Optional<Beach> optionalBeach,
             @RequestParam boolean wirelessInternet,
             @RequestParam boolean meal,
             @PathVariable("country_id") Long countryId) {
 
-        Country country = getCountryOrThrowNotFound(countryId);
+        Country country = controllersUtils.getCountryOrThrowNotFound(countryId);
+
+        if (seaResort && (optionalBeach.isEmpty() || optionalDistance.isEmpty())) {
+            return ResponseEntity.badRequest().build();
+        }
 
         Tour newTour = Tour
                 .builder()
                 .name(name)
                 .description(description)
                 .price(price)
-                .distance(distance)
                 .seaResort(seaResort)
-                .beach(beach)
                 .wirelessInternet(wirelessInternet)
                 .meal(meal)
                 .country(country)
                 .build();
 
+        if (seaResort) {
+            optionalBeach.ifPresent(newTour::setBeach);
+            optionalDistance.ifPresent(newTour::setDistance);
+        }
+
         tourService.save(newTour);
         return ResponseEntity
                 .created(linkTo(methodOn(TourController.class).getById(newTour.getId())).toUri())
-                .body(null);
-    }
-
-    @GetMapping("/tours/{country_id}")
-    public ResponseEntity<?> find(
-            @RequestParam Long cityId,
-            @PathVariable("country_id") Long countryId,
-            @RequestParam int days,
-            @RequestParam int people,
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Instant date) {
-
-        // Check if tour from City to Country available
-        Country country = getCountryOrThrowNotFound(countryId);
-
-        boolean available = cityService.findById(cityId)
-                .orElseThrow(() -> new CityNotFoundException(cityId))
-                .getCountries()
-                .contains(country);
-
-        if (available) {
-            List<Tour> tours = country.getTours();
-            Map<Long, Long> prices = new HashMap<>();
-            tours.forEach(t -> prices.put(t.getId(), t.getPrice() * people * days));
-
-            CollectionModel<Tour> tourCollectionModel = CollectionModel.of(
-                    tours,
-                    linkTo(
-                            methodOn(TourController.class)
-                                    .getAll())
-                            .withSelfRel());
-
-            TourDto tourDto = new TourDto(tourCollectionModel, prices, days, people, date);
-            return new ResponseEntity<>(
-                    tourDto,
-                    HttpStatus.OK);
-        } else {
-            City city = getCityOrThrowNotFound(cityId);
-            return new ResponseEntity<>(
-                    "Не удалось найти туров из " + city.getName() + " в " + country.getName(),
-                    HttpStatus.OK
-            );
-        }
+                .body(tourDtoFactory.createTourDto(newTour));
     }
 
     @DeleteMapping("/tours/{id}")
@@ -146,16 +87,6 @@ public class TourController {
                 .orElseThrow(() -> new TourNotFoundException(id));
 
         tourService.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    private Country getCountryOrThrowNotFound(Long countryId) {
-        return countryService.findById(countryId)
-                .orElseThrow(() -> new CountryNotFoundException(countryId));
-    }
-
-    private City getCityOrThrowNotFound(Long cityId) {
-        return cityService.findById(cityId)
-                .orElseThrow(() -> new CountryNotFoundException(cityId));
+        return ResponseEntity.ok().body(null);
     }
 }
